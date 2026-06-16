ae3ae186c2f5993c118dee3c17fd0ad501663464
zxxmwsj <1833436157@qq.com>
2026-06-16 18:21:28 +0800

    Site updated: 2026-06-16 18:21:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["articles/hello-world/"];function toRandomPost(){
+var posts=["articles/Webshare代理服务深度测评：性价比之王，低成本尝试住宅ip首选/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
