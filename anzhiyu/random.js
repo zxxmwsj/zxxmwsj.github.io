9f405a5312e054f0d9e68f81937db16b12f8d998
zxxmwsj <1833436157@qq.com>
2026-06-17 15:33:39 +0800

    Site updated: 2026-06-17 15:33:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["articles/港卡/","articles/Webshare代理服务深度测评：性价比之王，低成本尝试住宅ip首选/"];function toRandomPost(){
+var posts=["articles/2026-06-17/","articles/2026香港银行开户终极指南：汇丰、中银、ZA Bank 怎么选？/","articles/Webshare代理服务深度测评：性价比之王，低成本尝试住宅ip首选/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
