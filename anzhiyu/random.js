a9f9a9b5f20707e9eabb2445ec77d12b15efb2f5
zxxmwsj <1833436157@qq.com>
2026-07-04 16:20:49 +0800

    Site updated: 2026-07-04 16:20:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["articles/Webshare代理服务深度测评：性价比之王，低成本尝试住宅ip首选/"];function toRandomPost(){
+var posts=["articles/Starryblu 注册与使用指南：全球账户、多币种支付、银行卡申请全流程整理/","articles/Webshare代理服务深度测评：性价比之王，低成本尝试住宅ip首选/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
